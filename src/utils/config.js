537079e7d6ec04ff3838a9028adbf54f4bb96ad3
Juan Prasetyo <juanprasetyo000@gmail.com>
2023-11-17 09:49:32 +0700

    create config file

---
+++ src/utils/config.js
@@ -0,0 +1,15 @@
+/* eslint-disable comma-dangle */
+const config = {
+  app: {
+    host: process.env.HOST,
+    port: process.env.PORT,
+  },
+  rabbitMq: {
+    server: process.env.RABBITMQ_SERVER,
+  },
+  redis: {
+    host: process.env.REDIS_SERVER,
+  }
+};
+
+module.exports = config;
